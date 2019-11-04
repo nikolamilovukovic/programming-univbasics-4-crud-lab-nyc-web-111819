57216487295709b2e8fffc328a4e596ec182a85e
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-04 23:47:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["wow", "I", "am", "really", "learning"]
-  add_element_to_end_of_array << "element"
+  
+  array << "element"
   
 end
 
